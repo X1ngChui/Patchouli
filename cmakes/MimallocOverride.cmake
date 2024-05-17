195e5352b371ecb7fa82118a1fda20aba426afc0
X1ngChui <2209802631@qq.com>
2024-05-18 03:52:28 +0800

    Shared mimalloc

---
+++ cmakes/MimallocOverride.cmake
@@ -0,0 +1,10 @@
+function(mimalloc_override TARGET_NAME)
+    if (WIN32)
+        add_custom_command(
+            TARGET ${TARGET_NAME}
+            POST_BUILD
+            COMMAND ${PROJECT_SOURCE_DIR}/third-party/mimalloc/bin/minject -f -i $<TARGET_FILE:${TARGET_NAME}>
+            COMMENT "Inplace shared libraries for ${TARGET_NAME}..."
+        )
+    endif()
+endfunction()
